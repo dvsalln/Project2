129f4decc9daade26622299365ee2bd65e4ee4fd
Doc Ellis <DAVROGALL@ICLOUD.COM>
2018-08-30 20:40:44 -0700

    init houndify

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,14 +1,25 @@
 // DEPENDENCIES 
-var houndify = require('houndify');
-var express = require('express');
-var http = require('http');
-var config = require('./config');
+const houndify = require('houndify');
+const express = require('express');
+const http = require('http');
+const config = require('./config');
 
 // Create Express web app
-var app = express();
+const app = express();
 
 // Create an HTTP server and listen on the configured port
 var server = http.createServer(app);
 server.listen(config.port, function() {
     console.log('Express server listening on *:' + config.port);
 });
+
+var houndifyExpress = require('houndify').HoundifyExpress;
+
+//authenticates requests
+app.get('/houndifyAuth', houndifyExpress.createAuthenticationHandler({ 
+  clientId:  "YOUR_CLIENT_ID",
+  clientKey: "YOUR_CLIENT_KEY"
+}));
+
+//proxies text requests
+app.post('/textSearchProxy', houndifyExpress.createTextProxyHandler());
--- config.js
+++ config.js
@@ -1,10 +1,6 @@
   // HTTP Port to run our web application
   cfg.port = process.env.PORT || 3000;
   
-  // A random string that will help generate secure one-time passwords and
-  // HTTP sessions
-  cfg.secret = process.env.APP_SECRET || 'keyboard cat';
-  
   // Your Twilio account SID and auth token, both found at:
   // https://www.twilio.com/user/account
   // 
@@ -18,9 +14,6 @@
   // https://www.twilio.com/user/account/phone-numbers/incoming
   // Specify in E.164 format, e.g. "+16519998877"
   cfg.twilioNumber = process.env.TWILIO_NUMBER;
-  
-  // The sales rep / agent's phone number
-  cfg.agentNumber = process.env.AGENT_NUMBER;
   
   // Export configuration object
   module.exports = cfg;
